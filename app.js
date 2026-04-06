05d000ea08a23708dc66e4cd8174d1c9618b6f18
Maka322 <marciabaquero322@gmail.com>
2026-04-06 17:31:52 -0500

    Trabajo completo

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-
 const abecedario = [
-    { l: "A", img: "img/a.png", p: "Ángel" },
-    { l: "B", img: "img/b.png", p: "Barco" },
-    { l: "C", img: "img/c.png", p: "Casa" },
-    { l: "D", img: "img/d.png", p: "Dado" },
+    { l: "A", img: "img/a.png", p: "Alce" },
+    { l: "B", img: "img/b.png", p: "Búho" },
+    { l: "C", img: "img/c.png", p: "Cocodrilo" },
+    { l: "D", img: "img/d.png", p: "Delfín" },
     { l: "E", img: "img/e.png", p: "Elefante" },
-    { l: "F", img: "img/f.png", p: "Flor" },
-    { l: "G", img: "img/g.png", p: "Gato" },
-    { l: "H", img: "img/h.png", p: "Hielo" },
-    { l: "I", img: "img/i.png", p: "Iglesia" },
+    { l: "F", img: "img/f.png", p: "Foca" },
+    { l: "G", img: "img/g.png", p: "Gorila" },
+    { l: "H", img: "img/h.png", p: "Hipopótamo" },
+    { l: "I", img: "img/i.png", p: "Iguana" },
     { l: "J", img: "img/j.png", p: "Jirafa" },
     { l: "K", img: "img/k.png", p: "Koala" },
-    { l: "L", img: "img/l.png", p: "Luna" },
-    { l: "M", img: "img/m.png", p: "Manzana" },
-    { l: "N", img: "img/n.png", p: "Nube" },
-    { l: "Ñ", img: "img/nn.png", p: "Ñandú" }, // Usamos 'nn' para evitar problemas con la Ñ en archivos compañeros//
+    { l: "L", img: "img/l.png", p: "León" },
+    { l: "M", img: "img/m.png", p: "Marrano" },
+    { l: "N", img: "img/n.png", p: "Nutria" },
+    { l: "Ñ", img: "img/nn.png", p: "Ñandú" },
     { l: "O", img: "img/o.png", p: "Oso" },
     { l: "P", img: "img/p.png", p: "Perro" },
     { l: "Q", img: "img/q.png", p: "Queso" },
-    { l: "R", img: "img/r.png", p: "Ratón" },
-    { l: "S", img: "img/s.png", p: "Sol" },
-    { l: "T", img: "img/t.png", p: "Tren" },
-    { l: "U", img: "img/u.png", p: "Uva" },
+    { l: "R", img: "img/r.png", p: "Rana" },
+    { l: "S", img: "img/s.png", p: "Serpiente" },
+    { l: "T", img: "img/t.png", p: "Tucán" },
+    { l: "U", img: "img/u.png", p: "Unicornio" },
     { l: "V", img: "img/v.png", p: "Vaca" },
     { l: "W", img: "img/w.png", p: "Waffle" },
     { l: "X", img: "img/x.png", p: "Xilófono" },
@@ -29,29 +28,35 @@ const abecedario = [
     { l: "Z", img: "img/z.png", p: "Zebra" }
 ];
 
-// Visualización: Mostrar la imagen y el nombre de la letra seleccionada
-//
-
-function seleccionarLetra(letraPresionada) {
+function seleccionarLetra(letra) {
     const visor = document.getElementById("pantalla-imagen");
-    const encontrada = abecedario.find(item => item.l === letraPresionada);
-
-    if (encontrada) {
-         visor.innerHTML = `
-            <img src="${encontrada.img}" alt="${encontrada.p}" class="img-animada">
-            <h2>${encontrada.l} de ${encontrada.p}</h2>
+    const item = abecedario.find(a => a.l === letra);
+    if (item) {
+        visor.innerHTML = `
+            <img src="${item.img}" alt="${item.p}">
+            <h2>${item.l} de ${item.p}</h2>
         `;
     }
 }
 
-function cargarGaleria() {
-    const galeria = document.getElementById("galeria"); 
-    
-    abecedario.forEach(letra => {
-        const boton = document.createElement("button");
-        boton.innerText = letra.l;
-        boton.onclick = () => seleccionarLetra(letra.l);
-        galeria.appendChild(boton);
+function renderizarGaleria(lista) {
+    const galeria = document.getElementById("galeria");
+    galeria.innerHTML = ""; 
+    lista.forEach(item => {
+        const btn = document.createElement("button");
+        btn.innerText = item.l;
+        btn.className = "carta";
+        btn.onclick = () => seleccionarLetra(item.l);
+        galeria.appendChild(btn);
     });
 }
 
+function verTodoElABC() { renderizarGaleria(abecedario); }
+
+function verSoloVocales() {
+    const vocales = ["A", "E", "I", "O", "U"];
+    renderizarGaleria(abecedario.filter(a => vocales.includes(a.l)));
+}
+
+// Iniciar al cargar la página
+window.onload = verTodoElABC;
